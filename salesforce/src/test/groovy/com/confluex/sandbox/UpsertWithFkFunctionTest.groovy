6440ae8336cb5efa246594137f7e024dcce71be6
Mike Cantrell <mike@devnull.org>
2013-07-12 16:43:49 -0500

    success with customer SObject

--- salesforce/src/test/groovy/com/confluex/sandbox/UpsertWithFkFunctionTest.groovy
+++ salesforce/src/test/groovy/com/confluex/sandbox/UpsertWithFkFunctionTest.groovy
@@ -1,5 +1,6 @@
 package com.confluex.sandbox
 
+import com.sforce.soap.partner.sobject.SObject
 import org.junit.Test
 import org.mule.tck.junit4.FunctionalTestCase
 
@@ -12,20 +13,20 @@ class UpsertWithFkFunctionTest extends FunctionalTestCase {
 
     @Test
     void shouldUpsertRecordWithFk() {
+        def account = new SObject(type: "Account")
+        account.setField("ExternalId__c", 138)
         def payload = [
                 [
                         OriginalEmail__c: "mike@devnull.org",
                         FirstName: "Mike",
                         LastName: "Cantrell",
                         Email: "mike@devnull.org",
-                        Account: [
-                                ExternalId__c: 138
-                        ]
+                        Account: account
                 ]
         ]
         def msg = muleContext.client.send("upsert", payload, [:], 10000)
 
         assert !muleContext.client.request("errors", 1000)?.exceptionPayload?.exception
-        assert msg.payload
+        assert msg.payload[0].success == true
     }
 }
