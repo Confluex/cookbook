c9359a15cca98b34b9803ca3cf0e926c2e10089b
Mike Cantrell <mike@devnull.org>
2013-07-12 19:31:24 -0500

    Updated to support SObject or Map

--- salesforce/src/main/groovy/com/confluex/sandbox/MapToSalesForceObjectTransformer.groovy
+++ salesforce/src/main/groovy/com/confluex/sandbox/MapToSalesForceObjectTransformer.groovy
@@ -1,6 +1,7 @@
 package com.confluex.sandbox
 
 import com.sforce.soap.partner.sobject.SObject
+import com.sforce.ws.bind.XmlObject
 import groovy.util.logging.Slf4j
 import org.mule.api.transformer.TransformerException
 import org.mule.transformer.AbstractTransformer
@@ -10,18 +11,35 @@ class MapToSalesForceObjectTransformer extends AbstractTransformer {
 
     @Override
     protected Object doTransform(Object src, String enc) throws TransformerException {
-        def payload = src as List<Map>
-        payload.each { record ->
-            def types = record.findAll { it.value instanceof Map }
-            types.each { type ->
-                log.debug("Converting {} to SObject", type)
-                def sObject = new SObject(type: type.key.toString())
-                def fields = type.value as Map
-                fields.keySet().each { key -> sObject.setField(key.toString(), fields[key]) }
-                record[type.key] = sObject
-                log.debug("Conversion of {} to SObject complete: {}", type, sObject)
-            }
-        }
-        return payload
+        def list = src as List
+        list.each { map(it) }
     }
+
+    protected void map(Map record) {
+        def types = record.findAll { it.value instanceof Map }
+        types.each { type ->
+            log.debug("Converting {} to SObject", type)
+            def sObject = new SObject(type: type.key.toString())
+            def fields = type.value as Map
+            fields.keySet().each { key -> sObject.setField(key.toString(), fields[key]) }
+            record[type.key] = sObject
+            log.debug("Conversion of {} to SObject complete: {}", type, sObject)
+        }
+    }
+
+
+    protected void map(SObject record) {
+        def types = record.children.findAll { so -> record.getChild(so.name.localPart).value instanceof Map }
+        types.each { XmlObject so ->
+            def type = so.name.localPart
+            log.debug("Converting {} to SObject", type)
+            def sObject = new SObject(type: type)
+            def fields = so.value as Map
+            fields.keySet().each { key -> sObject.setField(key.toString(), fields[key]) }
+            record.setField(type, sObject)
+            log.debug("Conversion of {} to SObject complete: {}", type, sObject)
+        }
+
+    }
+
 }
--- salesforce/src/test/groovy/com/confluex/sandbox/MapToSalesForceObjectTransformerTest.groovy
+++ salesforce/src/test/groovy/com/confluex/sandbox/MapToSalesForceObjectTransformerTest.groovy
@@ -1,6 +1,7 @@
 package com.confluex.sandbox
 
 import com.sforce.soap.partner.sobject.SObject
+import com.sforce.ws.bind.XmlObject
 import org.junit.Test
 
 
@@ -22,4 +23,16 @@ class MapToSalesForceObjectTransformerTest {
         assert account.getChild("ExternalId__c").value == 138
     }
 
+    @Test
+    void shouldConvertEmbeddedMapsToSalesForceObjectsWithListOfSalesForceObjects() {
+        def payload = new SObject(type:"Contact")
+        payload.setField("OriginalName_c", "mike@devnull.org")
+        payload.setField("FirstName", "Mike")
+        payload.setField("LastName", "Cantrell")
+        payload.setField("Email", "mike@devnull.org")
+        payload.setField("Account", [ ExternalId__c:138])
+        def response = new MapToSalesForceObjectTransformer().doTransform([payload], null) as List<XmlObject>
+        def account = response[0].getField("Account") as XmlObject
+        assert account.getChild("ExternalId__c").value == 138
+    }
 }
